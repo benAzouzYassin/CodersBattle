458c3ee6dac3ae9cce3ed99df5f461befdbcb49b
benAzouzYassin <yassinebenazouz123@gmail.com>
2023-10-08 01:10:04 +0100

    refactored /updateUser route

--- app/api/updateUser/route.ts
+++ app/api/updateUser/route.ts
@@ -1,22 +1,29 @@
-//this api will get userId and useName and use leetCodeid
+
 import { db } from "@/firbaseService";
-import { doc, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, or, query, updateDoc, where } from "firebase/firestore";
 import { z } from "zod"
+import { fromZodError } from "zod-validation-error"
 
+type UserData = {
+    userName: string;
+    leetcodeId: string;
+    userId: string | undefined;
+}
 
-
-//TODO verify if leetcode id is taken or not 
+type FunctionResponse = {
+    success: boolean
+    message: string
+}
 
 export async function POST(req: Request) {
+
     const userData: UserData = await req.json()
-    const isValidRequest = verifyUserData(userData)
-    if (!isValidRequest) {
-        return Response.json({ "success": false, "message": "Invalid data " }, { status: 400 })
+
+    const { message, success } = await verifyUser(userData)
+    if (!success) {
+        return Response.json({ "success": false, "message": message }, { status: 500 })
     }
-    const isValidLeetcodeId = await verifyLeetcodeId(userData.leetcodeId ?? "")
-    if (!isValidLeetcodeId) {
-        return Response.json({ "success": false, "message": "Could not validate your leetcode id ! " }, { status: 400 })
-    }
+
     const targetUserRef = doc(db, "users", userData.userId ?? "")
     try {
 
@@ -33,32 +40,40 @@ export async function POST(req: Request) {
 }
 
 
-function verifyUserData(userData: any) {
-    const Schema = z.object({
-        userName: z.string().min(1),
-        userId: z.string().min(5),
-        leetcodeId: z.string().min(1),
-    });
+
+async function verifyUser(user: UserData): Promise<FunctionResponse> {
     try {
-        const data = Schema.parse({
-            userName: userData.userName,
-            leetcodeId: userData.leetcodeId,
-            userId: userData.userId
+
+        //verify user shape
+        const Schema = z.object({
+            userName: z.string().min(1, { message: "/user name is required !/" }),
+            userId: z.string().min(5, { message: "/invalid user id/" }),
+            leetcodeId: z.string().min(1, { message: "/leetcode name is required !/" }),
         });
-        return true;
-    } catch (error) {
-        return false;
+        const validationResult = Schema.safeParse({
+            userName: user.userName,
+            leetcodeId: user.leetcodeId,
+            userId: user.userId
+        });
+        if (!validationResult.success) {
+            const errorMessage = fromZodError(validationResult.error).message.split("/")[1]
+            return { "success": false, "message": errorMessage }
+        }
+
+        //check if leetcode name is real 
+        const leetcodeProfile = `https://leetcode.com/${user.leetcodeId}/`;
+        const leetcodeResponse = await fetch(leetcodeProfile, { method: "GET" });
+        if (leetcodeResponse.status != 200) return { success: false, message: "Invalid leetcode name !" }
+
+        //trying to query a user with same credentials
+        const usersInDB = collection(db, "users")
+        const q = query(usersInDB, or(where("name", "==", user.userName), where("leetCodeId", "==", user.leetcodeId)))
+        const res = (await getDocs(q)).size
+        if (res > 0) return { "success": false, message: "There is other user with the same user name / leetcode id please change one of them" }
+
+        return { success: true, message: "User was verified successfully !" }
+    } catch (error: any) {
+        console.log(error)
+        return { success: false, message: "Server error please try again later." }
     }
 }
-async function verifyLeetcodeId(leetcodeId: string) {
-    const leetcodeProfile = `https://leetcode.com/${leetcodeId}/`;
-    const res = await fetch(leetcodeProfile, { method: "GET" });
-    return res.status === 200;
-}
-
-type UserData = {
-    userName: string;
-    leetcodeId: string;
-    userId: string | undefined;
-}
-
